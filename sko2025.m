d35c5584ddf49820e733f67b94a9f84e5634bfa1
MikeAgostini <161889276+MikeAgostini@users.noreply.github.com>
2025-01-15 20:15:47 -0500

    Add files via upload

---
+++ sko2025.m
@@ -0,0 +1,12 @@
+disp(' ')
+disp('Running: ver') 
+ver
+disp(' ')
+disp('Running: rand(3)') 
+rand(3)
+disp(' ')
+disp('Running: why') 
+why
+disp(' ')
+disp('Running: system(''hostname'')') 
+system('hostname');
